ba1b68de13597e228b0b5e4505088fca0e910ef0
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-11-08 17:02:57 -0600

    added get routes to catergory-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,38 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  await Category.findAll({
+    attributes: ["id", "category_name"],
+        include: [{
+          model: Product,
+          attributes: ["id", "product_name", "price", "stock", "category_id"]
+        }]
+  }).then((categories) => {
+    res.json(categories);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+    await Category.findByPk(req.params.id, {
+      attributes: ["id", "category_name"],
+          include: [{
+            model: Product,
+            attributes: ["id", "product_name", "price", "stock", "category_id"],
+          }],
+    }).then((category) => {
+      res.json(category);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 router.post('/', (req, res) => {
