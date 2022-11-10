997d1512572ee0d7f5a551c819561eabb73c52ae
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-11-10 13:20:38 -0600

    added get routes to product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,52 @@
 const router = require('express').Router();
+const { includes } = require('lodash');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+    await Product.findAll({
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      include: [{
+        model: Tag,
+        attributes: ["id", "tag_name"],
+        through: "ProductTag"
+      },
+      {
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
+    ],
+    }).then((productData) => {
+      res.json(productData)
+    }).catch((err) => {
+      res.json(err)
+    })
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+    await Product.findByPk(req.params.id, {
+      include: [{
+        model: Tag,
+        attributes: ["id", "tag_name"],
+        through: "ProductTag",
+      },
+      {
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
+      ],
+    }).then((specificProduct) => {
+      res.json(specificProduct)
+    }).catch((err) => {
+      res.json(err)
+    })
 });
 
 // create new product
