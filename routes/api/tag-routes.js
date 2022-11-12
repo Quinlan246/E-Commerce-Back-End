6dee5d6b5dc83fff8e99725faeb3e89087ba7d00
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-11-12 11:18:46 -0600

    added get routes to tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,37 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+    await Tag.findAll({
+      attributes: ["id", "tag_name"],
+      include: [{
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+        through: "ProductTag",
+      }],
+    }).then((parsedTagData) => {
+      res.json(parsedTagData)
+    }).catch((err) => {
+      res.json(err)
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+    await Tag.findByPk(req.params.id, {
+      include: [{
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+        through: "ProductTag",
+      }],
+    }).then((receivedTag) => {
+      res.json(receivedTag)
+    }).catch((err) => {
+      res.json(err)
+    })
 });
 
 router.post('/', (req, res) => {
